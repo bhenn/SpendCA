3e8ef588d755c0c9f12f081d79b02d57b93a7d78
Bruno Henn <brunofhenn@gmail.com>
2019-02-07 12:10:03 -0500

    First step at login

---
+++ SpendCA.MVC/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SpendCA.Infrastructure.Data.Entities;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpendCA.MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager; 
+        }
+
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var result = _signInManager.PasswordSignInAsync(email, password, false, false).Result;
+
+            if (result.Succeeded)
+            {
+                var appUser = _userManager.Users.SingleOrDefault(r => r.Email == email);
+                var userLogged = new
+                {
+                    email = appUser.Email,
+                };
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("/");
+        }
+    }
+}
