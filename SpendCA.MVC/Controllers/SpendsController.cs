da9b59ac24a2802d624b9ba1a2397721af576a28
Bruno Henn <brunofhenn@gmail.com>
2019-02-08 16:15:47 -0500

    Create spend

--- SpendCA.MVC/Controllers/SpendsController.cs
+++ SpendCA.MVC/Controllers/SpendsController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SpendCA.Core.Entities;
 using SpendCA.Core.Interfaces;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,10 +16,12 @@ namespace SpendCA.MVC.Controllers
     public class SpendsController : Controller
     {
         private readonly ISpendRepository _spendsRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
-        public SpendsController(ISpendRepository spendRepository)
+        public SpendsController(ISpendRepository spendRepository, ICategoryRepository categoryRepository)
         {
             _spendsRepository = spendRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public IActionResult Index()
@@ -27,6 +30,30 @@ namespace SpendCA.MVC.Controllers
             var spends = _spendsRepository.GetAll(GetUserId());
 
             return View(spends);
+        }
+
+        public IActionResult Create()
+        {
+
+            ViewBag.Categories = _categoryRepository.GetAll(GetUserId());
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind] Spend spend)
+        {
+            if (ModelState.IsValid){
+
+                spend.UserId = GetUserId();
+                _spendsRepository.Add(spend);
+
+                return RedirectToAction("Index");
+            }
+
+
+            return View(spend);
         }
 
 
