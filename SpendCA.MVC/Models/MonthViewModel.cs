1815c3cc11f6bf5684e9a0e6d954c44735d11d79
Bruno Henn <brunofhenn@gmail.com>
2020-05-12 17:31:48 -0400

    Added new summary view by category with chart.js

---
+++ SpendCA.MVC/Models/MonthViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SpendCA.MVC.Models
+{
+    public class MonthViewModel
+    {
+        
+        public int Year { get; set; }
+        public int MonthId { get; set; }
+        public string Description { get; set; }
+        public double Total { get; set; }
+
+    }
+}
---
+++ SpendCA.MVC/Models/SummaryByCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpendCA.MVC.Models
+{
+    public class SummaryByCategoryViewModel
+    {
+        public string CategoryDescription { get; set; }
+        public List<MonthViewModel> Months;
+    }
+}
